15e8a5b02533f6bdae7f65bc76f2913c4a251f8a
Retreat-hunter <hunterkorsen@gmail.com>
2024-02-06 22:35:28 -0800

    done to the best of my ability right now as I did not dedicate enough time to this today.

---
+++ components/nav.php
@@ -0,0 +1,3 @@
+<div>
+    <a class="nav" href+="./create-duck.php">Create a Duck</a></div>
+<a href="./create-duck.php">
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <?php
+    include('components/head.php');
+    include('components/nav.php');
+    ?>
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<h2> 3-column grid layout</h2>
+	<div class="container">
+	   <div class="column">
+	     <h2> Bill </h2>
+		 <img class="./assets/images/Duck 1.png">
+            <ul>
+                <li>Pizza</li>
+                <li>Refried beans</li>
+                <li>Hubaba Gum</li>
+            </ul>
+        </div>
+	   <div class="column">
+	     <h2> Tony </h2>
+		<img class="./assets/images/Duck 2.png">
+            <ul>
+                <li>Nachos</li>
+                <li>Steak</li>
+                <li>Extra Gum Wrappers</li>
+            </ul>
+        </div>
+	   <div class="column">
+	     <h2> Reba </h2>
+		 <img class="./assets/images/Duck 3.png">
+            <ul>
+                <li>Dumplings</li>
+                <li>Apples</li>
+                <li>Left Airpod</li>
+            </ul>
+
+        </div>
+	</div>
+
+</body>
+</html>
